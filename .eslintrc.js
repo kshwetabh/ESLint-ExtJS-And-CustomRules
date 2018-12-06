119a0daa253585ab397890b86224771c68028c77
ksfnu <ksfnu@gmail.com>
2018-12-06 17:23:18 +0530

    project cleanup before uploading on github

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,53 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "extends": ["eslint:recommended", "plugin:sonarjs/recommended"],
-    "parserOptions": {
-        "ecmaVersion": 5
-    },
-    "globals": {
-        "document": false,
-        "window": false,
-        "Ext": false,
-        "EAM": false,
-        "infor": false,
-        "gMenuJson": false,
-        "gToolbarText": false,
-        "gUserData": false,
-        "gDebug": false,
-        "gAppData": false,
-        "gHyperlinkButtons": false,
-        "CustomEvent": false,
-        "vcei": false,
-        "UTILITY": false
-    },
-    "plugins": ["sonarjs", "hms-plugins"],
-    "rules": {
-        "indent": "off",
-        "linebreak-style": "off",
-        "quotes": "off",
-        "no-mixed-spaces-and-tabs": "off",
-        "semi": ["error", "always"],
-        "vars-on-top": "off", // "error",
-        "one-var": ["warn", "always"],
-        
-        //SonarLint rules
-        "sonarjs/no-duplicate-string": "off",
-        "sonarjs/cognitive-complexity": "off",
-        
-        //HMS Plugin rules
-        "hms-plugins/no-hms-ext-create-method": 2, 		//0=off, 1=warn, 2=error
-        "hms-plugins/no-hms-fieldlabel-asterisk": 2,
-        "hms-plugins/no-hms-ext-componentquery": 1,
-        "hms-plugins/no-hms-ext-window-autoshow-show": 2,
-        "hms-plugins/no-hms-servercall-async": 1,
-        "hms-plugins/no-hms-null-undefined": 0,
-        "hms-plugins/no-hms-lookup-start-LV": 1,
-        "hms-plugins/no-hms-bad-variable-names": 0, 	//Warning: very noisy rule, disabled by default
-        "hms-plugins/no-hms-bad-lov-config": 1 			//This needs to be a warning since LOV does not always need to have a type in modelFields
-    }
+  env: {
+    browser: true
+  },
+  extends: ["eslint:recommended", "plugin:sonarjs/recommended"],
+  parserOptions: {
+    ecmaVersion: 5
+  },
+  globals: {
+    document: false,
+    window: false,
+    Ext: false,
+    EAM: false,
+    infor: false,
+    gMenuJson: false,
+    gToolbarText: false,
+    gUserData: false,
+    gDebug: false,
+    gAppData: false,
+    gHyperlinkButtons: false,
+    CustomEvent: false,
+    vcei: false,
+    UTILITY: false
+  },
+  plugins: ["sonarjs", "hms-plugins"],
+  rules: {
+    indent: "off",
+    "linebreak-style": "off",
+    quotes: "off",
+    "one-var": "off",
+    "no-mixed-spaces-and-tabs": "off",
+    semi: ["error", "always"],
+    "vars-on-top": "off", // "error",
+    "one-var": "warn",
+    "no-lonely-if": "warn",
+    "no-loop-func": "warn",
+
+    //SonarLint rules
+    "sonarjs/no-duplicate-string": "off",
+    "sonarjs/cognitive-complexity": "off",
+
+    //HMS Plugin rules
+    "hms-plugins/no-hms-ext-create-method": 2, //0=off, 1=warn, 2=error
+    "hms-plugins/no-hms-fieldlabel-asterisk": 2,
+    "hms-plugins/no-hms-ext-componentquery": 1,
+    "hms-plugins/no-hms-ext-window-autoshow-show": 2,
+    "hms-plugins/no-hms-servercall-async": 1,
+    "hms-plugins/no-hms-null-undefined": 0,
+    "hms-plugins/no-hms-lookup-start-LV": 1,
+    "hms-plugins/no-hms-bad-variable-names": 0, //Warning: very noisy rule, disabled by default
+    "hms-plugins/no-hms-bad-lov-config": 1 //This needs to be a warning since LOV does not always need to have a type in modelFields
+  }
 };
--- readme.txt
+++ readme.txt
@@ -6,9 +6,9 @@
 ===============================================================================================================
  npm install -S ./eslint/eslint-plugin-hms-plugins
   
- node_modules\.bin\esw -w --clear --changed -c .eslintrc.js C:\Users\ksfnu\eclipseWorkspace\workspace38_Photon\Frontend\src\main\webapp\app\
+ node_modules\.bin\esw -w --clear --changed -c .eslintrc.js C:\eclipseWorkspace\Frontend\src\main\webapp\app\
 
 - To capture the performance of custom rules run the below command on Command Prompt inside LintTool directory.
 =============================================================================================================== 
 C:\> set TIMING=1
-C:\> node_modules\.bin\eslint --rule "hms-plugins/no-hms-ext-create-method: 2" --rule "hms-plugins/no-hms-fieldlabel-asterisk: 2" --rule "hms-plugins/no-hms-ext-componentquery: 1" --rule "hms-plugins/no-hms-ext-window-autoshow-show: 2" --rule "hms-plugins/no-hms-servercall-async: 1" --rule "hms-plugins/no-hms-null-undefined: 0" --rule "hms-plugins/no-hms-lookup-start-LV: 1" --rule "hms-plugins/no-hms-bad-variable-names: 0" --rule "hms-plugins/no-hms-bad-lov-config: 1"  C:\Users\ksfnu\eclipseWorkspace\workspace38_Photon\Frontend\src\main\webapp\app\
+C:\> node_modules\.bin\eslint --rule "hms-plugins/no-hms-ext-create-method: 2" --rule "hms-plugins/no-hms-fieldlabel-asterisk: 2" --rule "hms-plugins/no-hms-ext-componentquery: 1" --rule "hms-plugins/no-hms-ext-window-autoshow-show: 2" --rule "hms-plugins/no-hms-servercall-async: 1" --rule "hms-plugins/no-hms-null-undefined: 0" --rule "hms-plugins/no-hms-lookup-start-LV: 1" --rule "hms-plugins/no-hms-bad-variable-names: 0" --rule "hms-plugins/no-hms-bad-lov-config: 1"  C:\eclipseWorkspace\Frontend\src\main\webapp\app\
